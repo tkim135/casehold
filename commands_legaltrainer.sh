6c66c1ff9ecd6d81cc41549bd4298953fba7b057
Tony Kim <tony.kim@sambanovasystems.com>
2021-09-02 21:04:33 +0000

    Adapt to TADP finetune

--- commands_legaltrainer.sh
+++ commands_legaltrainer.sh
@@ -1,12 +1,12 @@
 python3 multiple_choice/run_mc_legaltrainer.py \
   --task_name casehold \
-  --model_name_or_path gpt2 \
+  --model_name_or_path gpt2-xl \
   --data_dir data/casehold \
   --do_train \
   --do_eval \
   --evaluation_strategy steps \
   --max_seq_length 128 \
-  --learning_rate 5e-6 \
+  --learning_rate 1e-5 \
   --num_train_epochs 3.0 \
   --output_dir logs/casehold/gpt \
   --overwrite_output_dir \
@@ -16,4 +16,7 @@ python3 multiple_choice/run_mc_legaltrainer.py \
   --per_device_train_batch_size 1 \
   --per_device_eval_batch_size 1 \
   --evaluation_strategy steps \
-  --eval_steps 5000
+  --weight_decay 0.01 \
+  --seed 42 \
+  --eval_steps 200 \
+  --weight "pytorch_model_lr1e-5_wd1.0_ckpt1680.bin"
